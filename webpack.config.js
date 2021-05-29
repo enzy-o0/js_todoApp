5f4ac60fe6b8b3cbfd555fa99f8615f135479939
ang_zy <oej3659@daum.net>
2021-05-29 22:55:59 +0900

    modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,17 @@ module.exports = {
             {
                 test: /\.js$/,
                 include: [
-                path.resolve(__dirname, 'src/js')
+                    path.resolve(__dirname, 'src/js')
                 ],
                 exclude: /node_modules/,
-                use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
-                    plugins: ['@babel/plugin-proposal-class-properties']
-                }
-                }
+                    use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                },
+                query: require('./.babelrc')
             },
         ]
     },
