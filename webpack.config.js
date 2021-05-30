0de2b33fb3fa4189bba11a4e8fb4978379b13134
ang_zy <oej3659@daum.net>
2021-05-30 12:22:38 +0900

    netlify error modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   // enntry file
@@ -8,6 +10,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
     },
+    devServer: {
+        port: 9000,
+    },
     module: {
         rules: [
             {
@@ -16,17 +21,24 @@ module.exports = {
                     path.resolve(__dirname, 'src/js')
                 ],
                 exclude: /node_modules/,
-                    use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
-                },
-                query: require('./.babelrc')
+                //     use: {
+                //     loader: 'babel-loader',
+                //     options: {
+                //         presets: ['@babel/preset-env'],
+                //         plugins: ['@babel/plugin-proposal-class-properties']
+                //     }
+                // },
+                // query: require('./.babelrc')
             },
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+            template: 'index.html',
+        }),
+        new Dotenv()
+    ],
     devtool: 'source-map',
     // https://webpack.js.org/concepts/mode/#mode-development
     mode: 'development'
